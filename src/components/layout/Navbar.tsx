0deba9b86de067a5d14a4ef7493dd7a4376bb2ac
bodruddozaredoy <bodruddozaredoy@gmail.com>
2025-10-16 02:48:20 +0600

    update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,6 +2,7 @@ import { IoIosArrowDown } from 'react-icons/io'
 import { LuBellDot } from 'react-icons/lu'
 import { Link } from 'react-router'
 import GoogleTranslateDropdown from './GoogleTranslateDropdown'
+import { Button } from '../ui/button'
 
 
 export default function Navbar() {
@@ -19,14 +20,15 @@ export default function Navbar() {
                 <LuBellDot className='text-xl'/>
             </div>
             <div className='flex gap-3 items-center'>
-                <div className='size-12 rounded-full overflow-hidden'>
+                {/* <div className='size-12 rounded-full overflow-hidden'>
                     <img className='size-full object-cover' src="https://res.cloudinary.com/dcrs7po93/image/upload/v1757882228/620829347212176702376_jmlddo.jpg" alt="" />
                 </div>
                 <div>
                     <h1 className='font-semibold text-black'>Bodruddoza Redoy</h1>
                     <p className='text-sm text-muted-foreground'>bodruddozaredoy@gmail.com</p>
                 </div>
-                <IoIosArrowDown/>
+                <IoIosArrowDown/> */}
+                <Button onClick={(e) => localStorage.removeItem("access-key")}>Log out</Button>
             </div>
         </div>
     </div>
