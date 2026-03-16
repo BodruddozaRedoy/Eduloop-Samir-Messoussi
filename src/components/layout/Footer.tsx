ad794d5baa3c2ed050b311a0db45b7a5e95163c5
Md Juyel Rana <155250069+juyel66@users.noreply.github.com>
2026-03-17 03:22:16 +0600

    Refactor Footer component to use anonymous function

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function Footer() {
+export default function () {
     return (
-        <div className='fixed bottom-0 left-0 w-full flex items-center justify-between text-muted-foreground px-6 py-3 bg-white border-t'>
+        <div className='flex items-center justify-between text-muted-foreground w-full mt-10'>
             <div>© Extra Handen--- School-safe. No personal data</div>
             <div>Privacy Policy</div>
         </div>
